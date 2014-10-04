e73a3cd1ac0c735c38b09e9e6ab649b0b26aa73b
Akshat <akshat@mit.edu>
2014-10-04 19:34:23 -0400

    added output

--- nw.py
+++ nw.py
@@ -2,8 +2,22 @@ import requests
 import math
 from PIL import Image
 import urllib2 as urllib
-import "process.py"
+import process
 import io
 
 def nose_stats(data):
-	
+	tags = data['photos'][0]['tags'][0]
+	cen=tags['nose']
+	cx=cen['x']
+	cy=cen['y']
+	fw=tags['width']
+	fh=tags['height']
+	print cx,cy
+	print fw,fh
+
+
+if __name__ == "__main__":
+	url="http://tinyurl.com/673cksr"
+	data=process.process_sky_biometry(url)
+	nose_stats(data)
+
