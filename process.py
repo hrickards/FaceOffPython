ed3896e80b5812d5d2db33ad8fc15268bec2084d
akscanb <akscanb@gmail.com>
2014-10-04 23:53:19 -0400

    chagnes to process.py

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ import math
 from PIL import Image, ImageDraw
 import urllib2 as urllib
 import io
-
+from numpy import *
 
 def process_image(image_url):
 	sb_data = analyze_sky_biometry(process_sky_biometry(image_url))
@@ -66,8 +66,83 @@ def eye_test(data,idata):
 	(iw,ih)=idata.size
 	la=(la[0]*iw/100.0,la[1]*ih/100.0)
 	lb=(lb[0]*iw/100.0,lb[1]*ih/100.0)
-	print la,lb
+	#print la,lb
 	save_image(draw_line(idata,la,lb))
+	
+	
+def bdfs(x, y, idata):
+	global bvis, bpink
+	
+	if(bvis[x][y]):
+		return 0
+	
+	bvis[x][y] = 1;
+	dx=[0, 0, 1, -1]
+	dy=[1, -1, 0, 0]
+	(iw,ih)=idata.size
+	
+	res = 1
+	
+	for g in range(0,4):
+		xx = x + dx[g]
+		yy = y + dy[g]
+		if(xx >= 0 and yy >= 0 and xx < iw and yy < ih):
+			if(bvis[xx][yy] == 0 and bpink[xx][yy] == 1):
+				res += bdfs(xx, yy, idata)
+				idata.putpixel((xx, yy), (0,255,0))
+				
+				
+	return res
+				
+	
+def eye_area(data, idata):
+	global bvis, bpink
+	bvis= [[0 for i in range(5000)] for j in range(5000)]
+	bpink=[[0 for i in range(5000)] for j in range(5000)]
+	tags = data['photos'][0]['tags'][0]
+	aaa = tags['eye_left']
+	bbb = tags['eye_right']
+	(iw,ih)=idata.size
+	(fw,fh)=face_size(data)
+	
+	for x in range(0, iw):
+		for y in range(0, ih):	
+			bpink[x][y] = 0
+			bvis[x][y] = 0
+	
+	for x in range(0, iw - 1):
+		for y in range(0, ih - 1):
+			(r,g,b)=idata.getpixel((x,y))
+			if(r > 110 and g > 90 and b > g + 10 and r > g - 10 and r < g + 25 and abs(b - g) < 50):
+				idata.putpixel((x, y), (255,0,255))
+				idata.putpixel((x + 1, y), (255,0,255))
+				idata.putpixel((x, y + 1), (255,0,255))
+				bpink[x][y] = 1;
+				bpink[x + 1][y] = 1;
+				bpink[x][y + 1] = 1;
+			if(r + g + b < 150 and abs(b - g) < 50 and abs(b -r) < 50):
+				idata.putpixel((x, y), (255,0,255))
+				idata.putpixel((x + 1, y), (255,0,255))
+				idata.putpixel((x, y + 1), (255,0,255))
+				bpink[x][y] = 1;
+				bpink[x + 1][y] = 1;
+				bpink[x][y + 1] = 1;
+				
+	
+	eye1 = bdfs(int(aaa['x'] * iw / 100), int(aaa['y'] * ih / 100), idata)
+	eye2 = bdfs(int(bbb['x'] * iw / 100), int(bbb['y'] * ih / 100), idata)
+	
+	eye1 /= (fw * fh) / 100
+	eye2 /= (fw * fh) / 100
+	
+	#print eye1, eye2
+	
+	#bdfs(int(b['x'] * iw / 100), int(b['y'] * ih / 100), idata)	
+	save_image(idata,"bojan.png")
+	
+	return (eye1 + eye2) / 2
+	
+	
 
 def face_size(result):
 	tags = result['photos'][0]['tags'][0]
@@ -87,9 +162,13 @@ def eye_distance(result):
 
 # If file is run directly
 if __name__ == "__main__":
-
-	idata=image_process("http://tinyurl.com/673cksr")
-	data=process_sky_biometry("http://tinyurl.com/673cksr")
-	eye_test(data,idata)
+	
+	#picture="http://i61.tinypic.com/21k9qba.jpg"
+	#picture="http://i58.tinypic.com/35a3gwk.jpg"
+	picture="http://i61.tinypic.com/xnursl.jpg"
+	
+	idata=image_process(picture)
+	data=process_sky_biometry(picture)
+	print eye_area(data,idata)
 
 
